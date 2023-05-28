468d0a5e798ca04e07802361be812e59a7bcc2e4
Chaa Loftin <chaamail@yahoo.com>
2023-05-27 21:54:52 -0400

    created veryfi integration stack

---
+++ lib/veryfi-integration/veryfi-integration-construct.ts
@@ -0,0 +1,84 @@
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { Duration } from 'aws-cdk-lib';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Construct }   from 'constructs';
+import { createResourceWithHyphenatedName } from "../util/helper";
+import { 
+    ISecurityGroup, 
+    IVpc,
+    Port,
+    SecurityGroup, 
+  } from 'aws-cdk-lib/aws-ec2';
+
+interface VeryfiIntegrationConstructProps {
+    env: {
+      region:  string
+    }
+    databaseSecurityGroup: ISecurityGroup;
+    stageName: string;
+    vpc: IVpc,
+}
+
+class VeryfiIntegrationConstruct extends Construct {
+  constructor(scope: Construct, id: string, props: VeryfiIntegrationConstructProps) {
+    super(scope, id);
+
+    const dlqName = createResourceWithHyphenatedName(props.env.region, props.stageName, 'VeryfiDocumentEventDLQ')
+    const queueName = createResourceWithHyphenatedName(props.env.region, props.stageName, 'VeryfiDocumentEventBrokerQueue')
+    const lambdaProducer = createResourceWithHyphenatedName(props.env.region, props.stageName, 'VeryfiDocumentEventProducerLambda')
+    const lambdaConsumer = createResourceWithHyphenatedName(props.env.region, props.stageName, 'VeryfiDocumentEventConsumerLambda')
+    const lambdaSecurityGroupName = createResourceWithHyphenatedName(props.env.region, props.stageName, 'VeryfiLambdaSecurityGroup')
+
+    // Create the Veryfi-document-event-producer Lambda function
+    const veryfiDocumentEventProducer = new lambda.Function(this, 'VeryfiDocumentEventProducer', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      timeout: Duration.seconds(30),
+      functionName: lambdaProducer,
+      vpc: props.vpc,
+      code: lambda.Code.fromAsset(`${process.env.CODEBUILD_SRC_DIR}/lib/veryfi-integration/lambda/event-producer`),
+    });
+
+    // Create the Veryfi-document-event-consumer Lambda function
+    const veryfiDocumentEventConsumer = new lambda.Function(this, 'VeryfiDocumentEventConsumer', {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: 'index.handler',
+        timeout: Duration.seconds(60),
+        functionName: lambdaConsumer,
+        vpc: props.vpc,
+        code: lambda.Code.fromAsset(`${process.env.CODEBUILD_SRC_DIR}/lib/veryfi-integration/lambda/event-consumer`),
+      });
+    
+    // Create the Veryfi-document-event-broker SQS queue and DLQ
+    const veryfiDocumentEventDLQ : Queue = new Queue(this, dlqName);
+    const veryfiDocumentEventBrokerQueue : Queue = new Queue(this, queueName, {
+      visibilityTimeout: Duration.minutes(5),
+      deadLetterQueue: {
+        queue: veryfiDocumentEventDLQ,
+        maxReceiveCount: 5
+      }
+    })
+
+    // Lambda security group
+    const veryfiIntegrationLambdaSecurityGroup = new SecurityGroup(this, lambdaSecurityGroupName, {
+        vpc: props.vpc,
+        description: 'Security group for the lambda functions that use Veryfi as an Ingress and oneXerp database as an Egress',
+        allowAllOutbound: true,
+      });
+  
+    // Get the security group from ID
+    const databaseSecurityGroup = SecurityGroup.fromSecurityGroupId(this, 'ImportedOneXerpDatabaseSecurityGroup', props.databaseSecurityGroup.securityGroupId);
+  
+    // Adds egress to the database security group, and ingress in the database security group from the veryfiIntegrationLambdaSecurityGroup    
+    veryfiIntegrationLambdaSecurityGroup.connections.allowTo(databaseSecurityGroup, Port.tcp(443), 'Allow Lambda endpoints to access the database');
+
+    // Create the event source mapping between the veryfiDocumentEventConsumer and the VeryfiDocumentEventBrokerQueue
+     veryfiDocumentEventConsumer.addEventSource(new SqsEventSource(veryfiDocumentEventBrokerQueue));
+
+    // Grant Producer and consumer permissions to Broker
+    veryfiDocumentEventBrokerQueue.grantSendMessages(veryfiDocumentEventProducer);
+    veryfiDocumentEventBrokerQueue.grantConsumeMessages(veryfiDocumentEventConsumer);
+  }
+}
+
