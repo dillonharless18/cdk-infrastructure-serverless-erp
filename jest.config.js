713619b9fd0b1aefbc8631e074c990d11a78f4b0
Chaa Loftin <chaamail@yahoo.com>
2023-08-13 19:40:08 -0400

    updated configs and gitignore

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    testEnvironment: "node",
+    roots: ["<rootDir>/test", "<rootDir>/lib/veryfi-integration/lambda/test"],
+    testMatch: ["**/*.test.ts", "**/*.test.js"],
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.js$": "babel-jest",
+        "^.+\\.cjs$": "babel-jest",
+    },
 };
