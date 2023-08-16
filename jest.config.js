bd85d090a961c57414d58083460e1d208fd9c5ed
Chaa Loftin <chaamail@yahoo.com>
2023-08-15 21:31:54 -0400

    updated jest config now that 30 tests are passing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     testEnvironment: "node",
+    moduleDirectories: ["node_modules"],
     roots: ["<rootDir>/test", "<rootDir>/lib/veryfi-integration/lambda"],
     testMatch: ["**/*.test.ts", "**/*.test.js"],
     transform: {
