0180309b3626f93b247537b31fa4d3da1860a679
Chaa Loftin <chaamail@yahoo.com>
2023-10-07 21:14:16 -0400

    added enums to their own file

--- lib/lambda-layers/onexerp-qb-layer/constants.ts
+++ lib/lambda-layers/onexerp-qb-layer/constants.ts
@@ -1,2 +1,16 @@
+export enum ActionType {
+    CREATE_PO = "CREATE_PO",
+    CREATE_VENDOR = "CREATE_VENDOR",
+    UPDATE_VENDOR = "UPDATE_VENDOR",
+    CREATE_CUSTOMER = "CREATE_CUSTOMER",
+    UPDATE_CUSTOMER = "UPDATE_CUSTOMER",
+    CREATE_JOB = "CREATE_JOB",
+    UPDATE_JOB = "UPDATE_JOB",
+}
+
+export enum ItemType {
+    Materials = "Materials",
+    Other = "Other",
+}
 
 
--- lib/lambda-layers/onexerp-qb-layer/contract.ts
+++ lib/lambda-layers/onexerp-qb-layer/contract.ts
@@ -1,17 +1,5 @@
-enum ActionType {
-    CREATE_PO = "CREATE_PO",
-    CREATE_VENDOR = "CREATE_VENDOR",
-    UPDATE_VENDOR = "UPDATE_VENDOR",
-    CREATE_CUSTOMER = "CREATE_CUSTOMER",
-    UPDATE_CUSTOMER = "UPDATE_CUSTOMER",
-    CREATE_JOB = "CREATE_JOB",
-    UPDATE_JOB = "UPDATE_JOB",
-}
-
-enum ItemType {
-    Materials = "Materials",
-    Other = "Other",
-}
+import { ActionType } from "./constants"
+import { ItemType } from "./constants";
 
 type Address = {
     Addr1: string
