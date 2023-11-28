c28ce9811f96b2a83593ac788b08eb97dae94577
Chaa Loftin <chaamail@yahoo.com>
2023-11-28 09:31:34 -0500

    updated comment

--- lib/veryfi-integration/veryfi-integration-construct.ts
+++ lib/veryfi-integration/veryfi-integration-construct.ts
@@ -67,7 +67,7 @@ export class VeryfiIntegrationConstruct extends Construct {
             props.stageName,
             "VeryfiLambdaSecurityGroup"
         );
-        // The bucket that will stored all images that are ingested by veryfi
+        // The bucket that will store all images that are ingested by veryfi
         const assetBucketArn = Fn.importValue("AssetBucketArnExport");
         const assetBucket = Bucket.fromBucketArn(
             this,
